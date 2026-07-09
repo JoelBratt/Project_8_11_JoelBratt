a841a9d7edf0b0c449dfce9a976aaa2622b252b9
Joel Bratt <jbratt1@student.cscc.edu>
2026-07-08 22:40:25 -0400

    display and open spots function

--- classes.py
+++ classes.py
@@ -20,3 +20,16 @@ class Board:
     #diag
     (0, 4, 8), (2, 4, 6)
     ]
+    
+    def display(self):
+        """Displays the game state to the console using print."""
+        print(
+        f' {self.spots[0]} | {self.spots[1]} | {self.spots[2]} \n'
+        f'----------\n'
+        f' {self.spots[3]} | {self.spots[4]} | {self.spots[5]} \n'
+        f'----------\n'
+        f' {self.spots[6]} | {self.spots[7]} | {self.spots[8]} \n')
+    
+    def get_open_spots(self):
+        """Returns the positions on the board that are yet to be taken"""
+        return [spot for spot in self.spots if isinstance(spot, int)]
